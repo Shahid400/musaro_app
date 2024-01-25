45a99938987db7fd4949def5807e6e56d1147d83
shahid400 <shahid.mehmood@ceative.co.uk>
2024-01-26 01:50:57 +0500

    architecture setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { AuthGuard } from './guards/auth.guard';
+import { ExceptionsFilter } from './filters';
+import { ResponseInterceptor } from './interceptors';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [
+    { provide: APP_GUARD, useClass: AuthGuard },
+    { provide: APP_FILTER, useClass: ExceptionsFilter },
+    { provide: APP_INTERCEPTOR, useClass: ResponseInterceptor },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,56 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { extractErrorMessages } from './shared/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get<ConfigService>(ConfigService);
+
+  /* CORS Configuration */
+  app.enableCors({
+    origin: '*',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        let errors: null | string[] = null;
+        validationErrors.forEach((error) => {
+          const errMessages = extractErrorMessages(error);
+          if (errMessages.length) {
+            errors = !errors?.length
+              ? [...errMessages]
+              : [...errors, ...errMessages];
+          }
+        });
+        return new BadRequestException(errors);
+      },
+    }),
+  );
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Musaro App')
+    .setDescription('API documentation for APP')
+    .addBearerAuth()
+    .setVersion('1.0')
+    .setExternalDoc('Postman Collection', '/api-json')
+    .build();
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('api', app, document);
+  const port = config.get('PORT') || 8000;
+  await app.listen(port);
 }
 bootstrap();
